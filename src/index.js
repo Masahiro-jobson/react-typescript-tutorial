0bc664057b43dc4136ad6c402a683fa6873123fe
masahiro <m.togasaki1026@gmail.com>
2026-02-10 08:58:13 +1100

    Review of Component instances

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-  return <h2>My First component</h2>;
-}
+// const Person = () => <h2>John Michael</h2>;
+// const Message = () => {
+//   return <p>This is my message</p>;
+// };
+
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <h2>image placeholder</h2>;
+const Title = () => <h2>Book Title</h2>;
+const Author = () => {
+  return <h4>Book Author</h4>;
+};
 
 // Importing React and ReactDOM and Javascript entry point
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList />);
 
-export default Greeting;
+export default BookList;
